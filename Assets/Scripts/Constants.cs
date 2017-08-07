0973941d95d5e0f22e90836096127d6b0673ac05
George Ntakakis <gntakakis@outlook.com>
2017-08-08 01:50:47 +0300

    Removed MS Key

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public class Constants {
 
-    //Microsoft Cognitive Services - 1/20/2017 5:34:33 PM
-    public static string MCS_COMPUTERVISIONKEY = "1874595cb86146beaf9e284610ddafe1"; // Computer Vision Key 1
+    public static string MCS_COMPUTERVISIONKEY = "---------Computer-Vision-Key---------";
 }

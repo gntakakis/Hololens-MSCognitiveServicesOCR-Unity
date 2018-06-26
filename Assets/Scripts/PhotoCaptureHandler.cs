0608baf7148973247add4bc8a49c4f8b3c8c385b
Giorgos Ntakakis <gntakakis@outlook.com>
2018-06-26 17:03:27 +0300

    UnityEngine.XR.WSA.Input

--- Assets/Scripts/PhotoCaptureHandler.cs
+++ Assets/Scripts/PhotoCaptureHandler.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.VR.WSA.WebCam;
 using System.Linq;
+using UnityEngine.XR.WSA.WebCam;
 
 public class PhotoCaptureHandler : MonoBehaviour
 {
